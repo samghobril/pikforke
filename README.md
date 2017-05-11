8707bff084028bcaf10b8940f027fd5cdf23156a
Sam Ghobril <samghobril@Sams-MacBook-Pro.local>
2017-05-11 10:54:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+![pikforke demo](out.gif?raw=true)
+
+# Pikforke
+> bash script to list the Pitchfork review scores for a certain musician/band.
+
+I've always had the habit of looking up Pitchfork review scores for musicians I'm thinking of checking out. This makes it easier/faster for me to do.
+
+## How to use
+1. Copy the [pikforke.sh](pikforke.sh) file and paste it into /usr/local/bin (or wherever your $PATH variable points to).
+2. Run pikforke.sh from the Terminal. You can also give the command the musician/band name as an argument, without waiting for the prompt, as such:
+> pikforke.sh "Kanye West"
+
+## Contribute
+I'm completely new to bash scripting, so I'd be happy to accept improvements and additional features.
---
+++ pikforke.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+grepFromUrl() {
+    if [ "$1" = "-u" ]; then
+	    curl -s $2 | hxpipe | grep -m 1 -B $4 -A $5 "$3"
+    else
+	    curl -s $1 | hxpipe | grep -B $3 -A $4 "$2"
+    fi
+	
+}
+
+
+if [ $# -lt 1 ] 
+then
+    read -p 'Artist: ' artist
+    artist=${artist// /%20}
+else
+    artist=${1// /%20}
+fi
+url="http://pitchfork.com/search/?query=$artist"
+artistUrl=$(grepFromUrl -u $url "artist-name" 7 0 | grep Ahref | grep -o "\/.*")
+fullUrl="http://pitchfork.com$artistUrl"
+albums=$(grepFromUrl $fullUrl "/reviews/albums/[0-9]" 1 1| grep -o "\/.*")
+for album in $albums
+do
+    fullUrl="http://pitchfork.com$album"
+    name=$(grepFromUrl $fullUrl "Aclass CDATA review-title$" 0 4 | grep "^\-")
+    name=${name:1}
+    score=$(grepFromUrl $fullUrl "Aclass CDATA score$" 0 4 | grep "^\-")
+    score=${score:1}
+    echo "$name    $score"
+done
